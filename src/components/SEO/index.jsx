ae6df9866f75dd550ff20f74305441074b384d3c
ytseo <ytseo@lgitpartners.com>
2025-01-20 15:37:52 +0900

    adsense

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,12 +9,14 @@ const SEO = ({ title, description, url }) => {
         <title>{blogName}</title>
       ) : (
         <title>{title + ' | ' + blogName}</title>
-      ) }
+      )}
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2327476184552798"
+              crossOrigin="anonymous"></script>
     </Helmet>
   )
 }
